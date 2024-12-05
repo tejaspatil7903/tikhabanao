0cc0a9a8baf93566766b7c15a0c9c006abdda86d
Tejas Patil <tejasvpatil03@gmail.com>
2024-12-05 16:05:55 +0530

    fifth

--- vite.config.js
+++ vite.config.js
@@ -4,7 +4,10 @@ import react from "@vitejs/plugin-react";
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  build: {
+    outDir: "public", // Set the build output directory to 'public'
+  },
   css: {
-    postcss: "./postcss.config.js", // This ensures that Vite uses the correct PostCSS configuration
+    postcss: "./postcss.config.js", // Ensure Vite uses the correct PostCSS configuration
   },
 });
